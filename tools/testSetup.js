46eb70bd91f928191c3aa3ff5dc42476ab3b9593
Gustavo <gfasequeira@gmail.com>
2016-12-24 16:52:25 -0300

    Change testSteup comments.

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -1,13 +1,6 @@
 // tests are placed alongside files under test.
 // this file does the following:
 // 1. registers babel for transpiling our code for testing
-// 2. disables webpack-specific features that mocha doesn't understand.
-// 3. requires jsdom so we can test via an in-memory dom in node
-// 4. sets up global vars that mimic a browser.
-
-// this assures the .babelrc dev config (which includes
-// hot module reloading code) doesn't apply for tests.
-process.env.node_env = 'test'
 
 // register babel so that it will transpile es6 to es5
 // before our tests run.
