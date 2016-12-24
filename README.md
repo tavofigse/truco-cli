ab835e3b4c94da382d49962af5c9cfbf63114773
Gustavo <gfasequeira@gmail.com>
2016-12-24 14:24:52 -0300

    Add standar js code style and some budgets.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # truco-cli
+
+[![Standard - JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+[![npm](https://img.shields.io/npm/l/express.svg)]()
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let screen = blessed.screen({
   smartCSR: true
 })
 
-screen.title = 'truco-cli 😎'
+screen.title = 'truco-cli 😎 '
 
 // Create a box perfectly centered horizontally and vertically.
 let box = blessed.box({
@@ -32,26 +32,14 @@ let box = blessed.box({
 // Append our box to the screen.
 screen.append(box)
 
-// Add a png icon to the box
-let icon = blessed.image({
-  parent: box,
-  top: 0,
-  left: 0,
-  type: 'overlay',
-  width: 'shrink',
-  height: 'shrink',
-  file: __dirname + '/my-program-icon.png',
-  search: false
-})
-
 // If our box is clicked, change the content.
-box.on('click', function(data) {
+box.on('click', data => {
   box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}')
   screen.render()
 })
 
 // If box is focused, handle `enter`/`return` and give us some more content.
-box.key('enter', function(ch, key) {
+box.key('enter', (ch, key) => {
   box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n')
   box.setLine(1, 'bar')
   box.insertLine(1, 'foo')
@@ -59,7 +47,7 @@ box.key('enter', function(ch, key) {
 })
 
 // Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+screen.key(['escape', 'q', 'C-c'], function (ch, key) {
   return process.exit(0)
 })
 
